402fc94bc973d284e83efecdb93e1b996ac66ff2
chungit201 <vuongvanchung30092001@gmail.com>
2022-09-14 10:01:50 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {SearchOutlined} from '@ant-design/icons';
+import type {InputRef} from 'antd';
+import {Button, Input, Space, Table} from 'antd';
+import type {ColumnsType, ColumnType} from 'antd/es/table';
+import type {FilterConfirmProps} from 'antd/es/table/interface';
+import React, {useEffect, useRef, useState} from 'react';
+import 'antd/dist/antd.min.css'
+// @ts-ignore
+import Highlighter from 'react-highlight-words';
+import axios from "axios";
+import moment from "moment";
 
-function App() {
+interface DataType {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
+
+type DataIndex = keyof DataType;
+
+
+const App: React.FC = () => {
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef<InputRef>(null);
+  const [whitelists, setWhitelists] = useState([])
+
+  useEffect(() => {
+    axios.get('https://slimeroyale.com/api/v1/user/whitelist')
+      .then(function (response) {
+        console.log(response);
+        setWhitelists(response.data.results)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+
+  }, [])
+
+
+
+
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+      width: '30%',
+    },
+    {
+      title: 'Type',
+      dataIndex: 'type',
+      key: 'type',
+      width: '20%',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+      sortDirections: ['descend', 'ascend'],
+    },
+    {
+      title: 'Created At',
+      render: (record) => {
+        return (
+          <div>{moment(record.createdAt).format('L')}</div>
+        )
+      },
+      key: 'email',
+      width: '30%',
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={"container d-flex align-items-center"}
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <div className={"w-100"}>
+        <h2 className={"text-center"}>Whitelist registration list : {whitelists.length} address</h2>
+        <Table className={"w-100 mt-3"} columns={columns} dataSource={whitelists}/>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
